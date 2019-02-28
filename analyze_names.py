9a0a993c759c132396a76740a609065db725ed56
Edvard Pedersen <edvard.pedersen@uit.no>
2019-02-28 16:17:41 +0100

    add common words and filter out low-frequency words

--- analyze_names.py
+++ analyze_names.py
@@ -15,6 +15,8 @@ output_string = "{:<20} {:<10} {:<10} {:<30}"
 print(output_string.format("Item", "Occurences", "Average", "Standard deviation"))
 for key in sort_list:
     count = len(item_counts[key])
+    if count < 5:
+        continue
     line_mean = mean(item_counts[key])
     line_variance = pstdev(item_counts[key], mu=line_mean)
     print(output_string.format(key, count, int(line_mean), int(line_variance)))
