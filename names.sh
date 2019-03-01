3efcc7d459a3febceaece421529774cb42650f20
Teo <teodor@dahlknutsen.no>
2019-03-01 11:59:39 +0100

    better list

--- names.sh
+++ names.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 
-cat names.dat | awk '{CMD[$1]++;count++} END { for (a in CMD) { if (CMD[a] >= 5) {print CMD[a] " " CMD[a]/count*100 "% " a; }}}' | grep -v "./" | column -c3 -s " " -t | sort -nr | head -n20
+cat names.dat | awk '{CMD[$1]++;count++} END { for (a in CMD) { if (CMD[a] >= 5) {print CMD[a] " " CMD[a]/count*100 "% " a; }}}' | grep -v "./" | column -c3 -s " " -t | sort -nr | head -n30
+
 
