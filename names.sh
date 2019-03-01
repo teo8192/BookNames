65edcb49928a650251616b4a5c353fb5626e03a9
Teo <teodor@dahlknutsen.no>
2019-03-01 11:51:45 +0100

    shell script to do it

---
+++ names.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+cat names.dat | awk '{CMD[$1]++;count++} END { for (a in CMD) { print CMD[a] " " CMD[a]/count*100 "% " a; }}' | grep -v "./" | column -c3 -s " " -t | sort -nr | head -n15
+
