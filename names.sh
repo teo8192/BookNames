e3a4bb867c9722b44a7fdd89bd0065c8ea9a639f
Teo <teodor@dahlknutsen.no>
2019-03-01 11:54:21 +0100

    updates

--- names.sh
+++ names.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-cat names.dat | awk '{CMD[$1]++;count++} END { for (a in CMD) { print CMD[a] " " CMD[a]/count*100 "% " a; }}' | grep -v "./" | column -c3 -s " " -t | sort -nr | head -n15
+cat names.dat | awk '{CMD[$1]++;count++} END { for (a in CMD) { if (CMD[a] >= 5) {print CMD[a] " " CMD[a]/count*100 "% " a; }}}' | grep -v "./" | column -c3 -s " " -t | sort -nr | head -n15
 
